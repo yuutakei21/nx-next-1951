4255c0f9145a183b7ca88abf9bc82aaa4d258494
yuu <yuutakei21@gmail.com>
2024-07-21 09:52:53 +0700

    add tailwind material

--- apps/next-web/tailwind.config.js
+++ apps/next-web/tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
