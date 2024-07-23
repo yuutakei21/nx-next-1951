7ec6b8479f919b2ec54c4daeb3e91bc320e8f05b
yuu <yuutakei21@gmail.com>
2024-07-23 09:53:50 +0700

    update

--- apps/next-web/src/app/components/molecules/NoticeAlert.tsx
+++ apps/next-web/src/app/components/molecules/NoticeAlert.tsx
@@ -9,7 +9,9 @@ function NoticeAlert({ description = 'description' }: any) {
   const [open, setOpen] = useState(true);
 
   return (
-    <div className="notice-alert border border-[#2ec946] w-full">
+    <div
+      className={`notice-alert ${open ? 'border' : ''} border-[#2ec946] w-full`}
+    >
       <Alert
         className="rounded-none py-1 border-l-4 border-[#2ec946] bg-transparent font-medium text-[#2ec946] items-center"
         open={open}
@@ -17,13 +19,13 @@ function NoticeAlert({ description = 'description' }: any) {
         action={
           <IconButton
             className="shadow-none rounded-2xl !absolute top-2 right-2 bg-[#2ec946] w-4 h-4"
-            // onClick={() => setOpen(false)}
+            onClick={() => setOpen(false)}
           >
             <FontAwesomeIcon className="text-white" icon={faClose} />
           </IconButton>
         }
       >
-        <Typography className='text-xs'>{description}</Typography>
+        <Typography className="text-xs">{description}</Typography>
       </Alert>
     </div>
   );
