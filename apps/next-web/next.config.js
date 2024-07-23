4ad578ba6ee69e3311d6bc77c0f5a888acfdced9
yuu <yuutakei21@gmail.com>
2024-07-23 12:02:16 +0700

    update config buid

--- apps/next-web/next.config.js
+++ apps/next-web/next.config.js
@@ -1,17 +1,43 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+const { composePlugins, withNx,  } = require('@nx/next');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
+
 const nextConfig = {
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  // compiler: {
+  //   // For other options, see https://styled-components.com/docs/tooling#babel-plugin
+  //   styledComponents: true,
+  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@material-tailwind/react'],
+  },
+  output: 'standalone',
+  // webpack: (
+  //   config,
+  //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  // ) => {
+  //   // Important: return the modified config
+  //   return config;
+  // },
 };
 
 // if (process.env.STATIC === 'true') {
