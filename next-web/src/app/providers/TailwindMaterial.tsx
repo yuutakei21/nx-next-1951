3c1d5a708e2ba1a42494ac5d65d6c6eb274b90e4
yuu <yuutakei21@gmail.com>
2024-07-26 19:31:08 +0700

    update

--- next-web/src/app/providers/TailwindMaterial.tsx
+++ next-web/src/app/providers/TailwindMaterial.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { ThemeProvider } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import { materialTheme } from "../theme";
 
 export default function TailwindMaterial({
   children,
 }: Readonly<{
   children: any;
 }>) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    console.log("check  document");
+    if (document && !loaded) setLoaded(true);
+  }, []);
+
+  return (
+    <ThemeProvider value={materialTheme}>
+      <div className="app-root min-h-screen">
+        {loaded && <ThemeProvider>{children}</ThemeProvider>}
+      </div>
+    </ThemeProvider>
+  );
 }
