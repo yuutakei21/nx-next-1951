445637a1bd61b0febae418581a3cec3e3117fd34
yuu <yuutakei21@gmail.com>
2024-08-07 19:47:29 +0700

    update

--- flowbite-react-app/src/App.tsx
+++ flowbite-react-app/src/App.tsx
@@ -1,10 +1,12 @@
 import { DarkThemeToggle } from "flowbite-react";
 
-export function App() {
+function App() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">Flowbite React + Parcel</h1>
+      <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
       <DarkThemeToggle />
     </main>
   );
 }
+
+export default App;
